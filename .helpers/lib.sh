f642a40666f5c0179578a6cd0417047fd3160be6
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2025-09-28 12:00:13 +0200

    chore: empty format returns the whole JSON. If we do not care about the answer, give it an non empty string

--- .helpers/lib.sh
+++ .helpers/lib.sh
@@ -1,5 +1,5 @@
 _container_exists() { 
-    docker inspect --format '' $1 >/dev/null 2>&1
+    docker inspect --format ' ' $1 >/dev/null 2>&1
 }
 _container_status() { 
     docker inspect --format '{{.State.Status}}' $1 2>/dev/null
